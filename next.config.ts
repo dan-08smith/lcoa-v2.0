002bcd06e69d918790bc2e907cb5778b2149c08b
dan3289 <157528938+dan-08smith@users.noreply.github.com>
2025-02-14 05:56:56 +0000

    Prepare AWS Build Configs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  output: "standalone", // Enables standalone mode for optimized builds
+  images: {
+    domains: ["example.com"], // Add any external image domains if needed
+  },
 };
 
 export default nextConfig;
